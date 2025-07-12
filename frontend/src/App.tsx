c0a292be1ad037c8bbceea2666b262341e3b82dc
geeeezx <geeeeezx0402@gmail.com>
2025-07-12 20:40:38 +0800

    add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import SearchView from './components/SearchView';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HomePage, ExplorePage } from './pages';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/explore",
+    element: <ExplorePage />,
+  },
+]);
 
 function App() {
   return (
     <div className="min-h-screen bg-background">
-      <SearchView />
+      <RouterProvider router={router} />
     </div>
   );
 }
